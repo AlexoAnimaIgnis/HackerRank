4cf2b54fe8d434355fd2005ac3d71cafa45a0bd7
unknown <ballester.alexander@gmail.com>
2020-07-14 15:48:40 +0800

    loop collections usinng iterator

---
+++ src/main/com/java/collections/CollectionConcepts.java
@@ -0,0 +1,27 @@
+package main.com.java.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionConcepts {
+
+    public static void main (String... args) {
+        Product door = new Product("Wooden door", 35);
+        Product floorpanel = new Product("Floor Panel", 25);
+        Product window = new Product("Glass Window", 10);
+
+        Collection<Product> products = new ArrayList<>();
+        products.add(door);
+        products.add(floorpanel);
+        products.add(window);
+
+        /** Using iterator interface */
+        final Iterator<Product> productIterator = products.iterator();
+        while(productIterator.hasNext()) {
+            Product product = productIterator.next();
+            System.out.println(product);
+        }
+
+    }
+}
--- src/main/com/java/collections/SetDriver.java
+++
@@ -1,4 +0,0 @@
-package main.com.java.collections;
-
-public class SetDriver {
-}
