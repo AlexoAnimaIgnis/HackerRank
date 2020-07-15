5ee243a5531a9369764fcbc9e15d90ceeb0ef3c6
unknown <ballester.alexander@gmail.com>
2020-07-15 15:53:20 +0800

    use comparator interface for TREE SET

--- src/main/com/java/collections/ProductCatalogue.java
+++ src/main/com/java/collections/ProductCatalogue.java
@@ -1,12 +1,12 @@
 package main.com.java.collections;
 
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class ProductCatalogue implements Iterable<Product> {
 
-    private final Set<Product> productSet  = new HashSet<>();
+    private final Set<Product> productSet  = new TreeSet<>(Product.BY_NAME);
 
     public void isSuppliedBy(Supplier supplier) {
         productSet.addAll(supplier.getProducts());
