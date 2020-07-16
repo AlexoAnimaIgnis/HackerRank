95551e66b1733e9e8f819235ea6311a41de39eae
unknown <ballester.alexander@gmail.com>
2020-07-16 16:25:16 +0800

    implemented additional methods that uses PEEK and remove

--- src/main/com/java/collections/HelpDesk.java
+++ src/main/com/java/collections/HelpDesk.java
@@ -11,9 +11,30 @@ public class HelpDesk {
         enquiries.offer(new Enquiry(customer, category));
     }
 
+    public void processPrinterEnquiry() {
+        final Enquiry enquiry = enquiries.peek();
+        if(enquiry != null && enquiry.getCategory() == Category.PRINTER){
+            enquiries.remove();
+            enquiry.getCustomer().reply("Is it out of paper?");
+        } else {
+            System.out.println("No work to do, let's have some coffee!");
+        }
+    }
+
+    public void processGeneralEnquiry() {
+        final Enquiry enquiry = enquiries.peek();
+        if(enquiry != null && enquiry.getCategory() != Category.PRINTER){
+            enquiries.remove();
+            enquiry.getCustomer().reply("Have you tried turning it on and off again?");
+        } else {
+            System.out.println("No work to do, let's have some coffee");
+        }
+    }
+
     public void processAllEnquiries() {
-        while (!enquiries.isEmpty()) {
-            final Enquiry enquiry = enquiries.remove();
+        Enquiry enquiry;
+
+        while ((enquiry = enquiries.poll()) != null) {
             enquiry.getCustomer().reply("Have you tried turning it off and off again? ");
         }
     }
@@ -24,6 +45,8 @@ public class HelpDesk {
         helpDesk.enquire(Customer.JACK, Category.PHONE);
         helpDesk.enquire(Customer.JILL, Category.PRINTER);
 
-        helpDesk.processAllEnquiries();
+        helpDesk.processPrinterEnquiry();
+        helpDesk.processGeneralEnquiry();
+        helpDesk.processPrinterEnquiry();
     }
 }
