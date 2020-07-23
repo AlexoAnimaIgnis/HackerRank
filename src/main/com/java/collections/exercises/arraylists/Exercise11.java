4a1aabaddb8770d4d92978c7f00c10a61da16446
unknown <ballester.alexander@gmail.com>
2020-07-23 23:00:03 +0800

    Write a Java program to reverse elements in a array list

--- src/main/com/java/collections/exercises/arraylists/Exercise11.java
+++ src/main/com/java/collections/exercises/arraylists/Exercise11.java
@@ -1,4 +1,28 @@
 package main.com.java.collections.exercises.arraylists;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Write a Java program to reverse elements in a array list
+ */
 public class Exercise11 {
+    public static void main(String... args) {
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        System.out.println("Original List:");
+        numbers.stream().forEach( i -> { System.out.println(i); });
+
+        System.out.println("Reversed List:");
+        Collections.reverse(numbers);
+        numbers.stream().forEach(i->{System.out.println(i);});
+
+    }
 }
