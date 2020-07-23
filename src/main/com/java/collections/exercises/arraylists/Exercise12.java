18d7328c1ce6fe7bc3a5bcf0fd00037d081a5270
unknown <ballester.alexander@gmail.com>
2020-07-23 23:15:56 +0800

    Write a Java program to extract a portion of a array list

--- src/main/com/java/collections/exercises/arraylists/Exercise12.java
+++ src/main/com/java/collections/exercises/arraylists/Exercise12.java
@@ -1,4 +1,25 @@
 package main.com.java.collections.exercises.arraylists;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Write a Java program to extract a portion of a array list
+ */
 public class Exercise12 {
+    public static void main(String... args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        System.out.println("Original list: ");
+        numbers.stream().forEach(i->{System.out.println(i);});
+
+        System.out.println("Extracted List");
+        List<Integer> extractedNumber = numbers.subList(0, 2);
+        extractedNumber.stream().forEach(integer -> {System.out.println(integer);});
+
+    }
 }
